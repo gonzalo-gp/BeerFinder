a194081c361683b5b441902631ac68524f6b4a6c
Arvid Nydén <arvid.nyden@avanade.com>
2021-02-04 08:48:46 +0100

    updated readme

--- Database/Product.sql
+++ Database/Product.sql
@@ -8,9 +8,11 @@
     [Producer] NVARCHAR(250) NULL, 
     [Country] NVARCHAR(250) NULL, 
     [IsEco] BIT NULL, 
-    [AlcoholPercentage] INT NULL, 
+    [AlcoholPercentage] DECIMAL(14, 2) NULL, 
     [ImageUrl] NVARCHAR(250) NULL, 
     [Description] NVARCHAR(MAX) NULL, 
     [EAN] NVARCHAR(250) NULL, 
-    [ExternalProcductId] NVARCHAR(250) NULL
+    [ExternalProductId] NVARCHAR(250) NULL, 
+    [StockStatus] NVARCHAR(250) NULL, 
+    [Shelf] NVARCHAR(250) NULL
 )
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # beerfinder-api
+
+### TODO API
+* Add Product Rating to DB and Model
+* Add GTIN to to DB and Model
+* Add Update and Delete methods for setting and product
+* Get it working in Azure, depoloyed from Visual Studio
+* Add a Blazor frontend for managing products and settings
+  * https://codewithmukesh.com/blog/blazor-crud-with-entity-framework-core/
+  * https://docs.microsoft.com/en-us/aspnet/core/blazor/?view=aspnetcore-5.0
