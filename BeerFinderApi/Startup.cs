b7dbcb6cde4763e68a10d7794b5f053a3e5cb8d2
gonzalo-gp <g.a.gonzalez.pascual@avanade.com>
2021-03-03 12:42:18 +0100

    work around to access from Redux app

--- BeerFinderApi/Startup.cs
+++ BeerFinderApi/Startup.cs
@@ -29,7 +29,15 @@ namespace BeerFinderApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
             services.AddRazorPages();
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -45,6 +53,8 @@ namespace BeerFinderApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseCors("CorsPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
